e2c68bbd1f369299448db9f776e6df1f0030d91d
Marika <marikashanahan@gmail.com>
2021-03-06 22:02:55 -0700

    Create Bookings.js and base filter methods

--- src/Bookings.js
+++ src/Bookings.js
@@ -2,4 +2,12 @@ export default class Bookings {
   constructor(allBookings) {
     this.bookings = allBookings
   }
+
+  bookingsByDate (desiredDate) {
+    return this.bookings.filter(booking => booking.date === desiredDate)
+  }
+
+  bookingsByUser (desiredUserID) {
+    return this.bookings.filter(booking => booking.userID === desiredUserID)
+  }
 }
