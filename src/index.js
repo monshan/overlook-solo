60da0f89b64d3cc45e172ba393def5c01b09a575
Marika <marikashanahan@gmail.com>
2021-03-07 23:09:02 -0700

    Successful API get requests

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
 import Swal from 'sweetalert2'
 import './css/base.scss';
+import Rooms from './Rooms'
+import Bookings from './Bookings'
 
-const testBtn = document.getElementById('testBtn')
-testBtn.addEventListener('click', fireSA)
-
-function fireSA () {
-  Swal.fire('Testing number two')
-}
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 
-// getRooms = () => {
-//   fetch()
-//     .then(response => response.json())
-// }
+const getRooms = () => {
+  return fetch('http://localhost:3001/api/v1/rooms')
+    .then(response => response.json())
+}
+
+const getBookings = () => {
+  return fetch('http://localhost:3001/api/v1/bookings')
+    .then(response => response.json())
+}
+
+const loadGlobals = () => {
+  Promise.all([getRooms(), getBookings()])
+    .then(([loadedRooms, loadedBookings]) => {
+      globalRooms = new Rooms (loadedRooms)
+      globalBookings = new Bookings (loadedBookings)
+    })
+}
+
+loadGlobals();
