4865c08ed56b06112661c1876a8be0d7cc88a634
Marika <marikashanahan@gmail.com>
2021-03-07 22:09:53 -0700

    Apply flex grid to body

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 // An example of how you tell webpack to use a CSS (SCSS) file
+import Swal from 'sweetalert2'
 import './css/base.scss';
 
+const testBtn = document.getElementById('testBtn')
+testBtn.addEventListener('click', fireSA)
+
+function fireSA () {
+  Swal.fire('Testing number two')
+}
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 
